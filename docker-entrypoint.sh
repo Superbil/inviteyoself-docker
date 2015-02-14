df434796b5340b7197a4809d467ca9dd380cb062
Superbil <superbil@gmail.com>
2015-02-15 01:25:58 +0800

    Fix use wrong env name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@ export DB_NAME=$PGDB_ENV_POSTGRES_USER
 export DB_USER_NAME=$PGDB_ENV_POSTGRES_USER
 export DB_USER_PASSWORD=$PGDB_ENV_POSTGRES_PASSWORD
 
-echo "Build With jdbc:postgresql://$DB_HOST:$DB_POST/$DB_USER_NAME?user=$DB_USER_NAME&password=$DB_USER_PASSWORD"
-lein ragtime migrate -d "jdbc:postgresql://$DB_HOST:$DB_POST/$DB_USER_NAME?user=$DB_USER_NAME&password=$DB_USER_PASSWORD"
+echo "Build With jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME?user=$DB_USER_NAME&password=$DB_USER_PASSWORD"
+lein ragtime migrate -d "jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME?user=$DB_USER_NAME&password=$DB_USER_PASSWORD"
